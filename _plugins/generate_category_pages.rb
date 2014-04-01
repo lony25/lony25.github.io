e9bf267064075b91b43baae7727688bae4f77fbe
Lokesh <lokesh@3loq.com>
2014-04-01 12:17:06 +0530

    multi blog site testing

--- _plugins/generate_category_pages.rb
+++ _plugins/generate_category_pages.rb
@@ -1,39 +1,5 @@
 module Jekyll
 
-  class Site
-
-    attr_accessor :articles
-
-		def reset
-			self.articles = Array.new
-		end
-
-		alias_method :site_payload_articles, :site_payload
-		def site_payload
-			p = site_payload_articles
-      p['site']['articles'] = self.articles
-			p
-		end
-
-		alias_method :read_posts_articles, :read_posts
-		def read_posts(dir)
-
-      self.articles = Array.new
-
-			read_posts_articles dir
-			self.posts.each do |p| 
-      
-        if (not p.categories.include?('statuses') and not p.categories.include?('checkins') and not p.categories.include?('events'))
-          self.articles << p
-        end        
-      
-			end
-
-			self.articles.sort_by! { |p| -p.date.to_f }
-
-		end
-  end
-
   class Pagination < Generator
     def generate(site)
     end
@@ -63,11 +29,10 @@ module Jekyll
       (1..pages).each do |num_page|
       
         # the CategoryPager handles the paging and category data
-        pager = CategoryPager.new(site.config, num_page, category_posts, page.data['category'], pages)
+        pager = CategoryPager.new(site, num_page, category_posts, page.data['category'], pages)
 
-        # the first page is the index, so no page needs to be created. However, the subsequent pages need to be generated
         if num_page > 1
-          newpage = CategorySubPage.new(site, site.source, page.data['category'], page.data['category_layout'])
+          newpage = Page.new(site, site.source, page.dir, page.name)
           newpage.pager = pager
           newpage.dir = File.join(page.dir, "/page#{num_page}")
           site.pages << newpage
@@ -89,9 +54,9 @@ module Jekyll
     end
     
     # same as the base class, but includes the category value
-    def initialize(config, page, all_posts, category, num_pages = nil)
-    	@category = category
-      super config, page, all_posts, num_pages
+    def initialize(site, page, all_posts, category, num_pages = nil)
+      @category = category
+      super site, page, all_posts, num_pages
     end
 
     # use the original to_liquid method, but add in category info
@@ -104,55 +69,33 @@ module Jekyll
     
   end
   
-  # The CategorySubPage class creates a single category page for the specified tag.
-  # This class exists to specify the layout to use for pages after the first index page
-  class CategorySubPage < Page
-    
-    def initialize(site, base, category, layout)
-        
-      @site = site
-      @base = base
-      @dir  = category
-      @name = 'index.html'
-
-      self.process(@name)
-      self.read_yaml(File.join(base, '_layouts'), layout || 'category_index.html')
-
-      title_prefix             = site.config['cateogry_title_prefix'] || 'Everything in the '
-      self.data['title']       = "#{title_prefix}#{category}"
-
-    end
-    
-  end
-
-  
   module Filters
   
-  	def pager_links(pager)
+    def pager_links(pager)
 
-		if pager['previous_page'] || pager['next_page']
-  	  	
-			html = '<div class="pager clearfix">'
-			if pager['previous_page']
+    if pager['previous_page'] || pager['next_page']
+        
+      html = '<div class="pager clearfix">'
+      if pager['previous_page']
+        
+        if pager['previous_page'] == 1
+          html << "<div class=\"previous\"><a href=\"/#{pager['category']}/\">&laquo; Newer posts</a></div>"
+        else
+          html << "<div class=\"previous\"><a href=\"/#{pager['category']}/page#{pager['previous_page']}\">&laquo; Newer posts</a></div>"
+        end
+  
+      end
+  
+      if pager['next_page'] 
+        html << "<div class=\"next\"><a href=\"/#{pager['category']}/page#{pager['next_page']}\">Older posts &raquo;</a></div>"
+      end
+      
+      html << '</div>'
+      html
 
-				if pager['previous_page'] == 1
-					html << "<div class=\"previous\"><a href=\"/#{pager['category']}/\">&laquo; Newer posts</a></div>"
-				else
-					html << "<div class=\"previous\"><a href=\"/#{pager['category']}/page#{pager['previous_page']}\">&laquo; Newer posts</a></div>"
-				end
+    end
 
-			end
-
-			if pager['next_page'] 
-				html << "<div class=\"next\"><a href=\"/#{pager['category']}/page#{pager['next_page']}\">Older posts &raquo;</a></div>"
-			end
-
-			html << '</div>'
-			html
-
-		end
-
-  	end
+    end
   
   end
 
